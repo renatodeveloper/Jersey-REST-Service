5c68728006e66241f2b0dda40a4ae99dff1f2161
Renato <renato_developer@hotmail.com>
2017-02-10 17:39:43 -0200

    Testes

--- RestFul/src/br/com/restful/resource/UsuarioResource.java
+++ RestFul/src/br/com/restful/resource/UsuarioResource.java
@@ -1,19 +1,17 @@
 package br.com.restful.resource;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 
 import br.com.restful.controler.UsuarioControler;
-
-
 /**
  * Classe responsavel por conter os metodos rest de acesso ao webservice
  */
-
-
-
 @Path("/usuario")
 public class UsuarioResource {
 	/**
@@ -22,18 +20,58 @@ public class UsuarioResource {
 	 * @return Boolean usuario
 	 */
 	
-	@GET
-	@Path("/login")
+	
+	@POST
+	@Path("/login/stringImplicito")
 	@Produces("application/json")
-//	@Produces("aplication/xml")
-	
-	//http://localhost:8080/RestFul/usuario/login?param1=developer&param2=developer
-	//http://localhost:8080/RestFul/usuario/login?paramJsonUser={"usuario":{"dsLogin": "developer","dsSenha": "developer"}}
-	
-	
-	public String validarUsuario(@Context UriInfo info){
+	//@Produces("aplication/xml")
+	public String getStatusLoginStringImplicito(@Context UriInfo info, @QueryParam ("paramJsonUser") String paramJsonUser){
 		String result = "NOK";
 		String json = info.getQueryParameters().getFirst("paramJsonUser");
+		try{
+			if(json != null && json.length()>0){
+				if(json.equals("Implicito")){
+					return "OK_Implicito";
+				}
+				return "NOK_Implicito";
+			}
+		}catch(Exception e){
+			e.getMessage().toString();
+		}
+	
+		return result;
+	}
+	
+	
+	@GET
+	@Path("/login/users/{username}")
+	@Produces("application/json")
+	//@Produces("aplication/xml")
+	public String getStatusLoginStringExplicito(@Context UriInfo info, @PathParam ("username") String paramJsonUser){
+		String result = "NOK";
+		String json = info.getPathParameters().getFirst("username");
+		try{
+			if(json != null && json.length()>0){
+				if(json.equals("Explicito")){
+					return "Explicito";
+				}
+				return "NOK_Explicito";
+			}
+		}catch(Exception e){
+			e.getMessage().toString();
+		}
+	
+		return result;
+	}
+	
+	
+	@GET
+	@Path("/login/users/json/{paramJsonUser}")
+	@Produces("application/json")
+	//@Produces("aplication/xml")
+	public String getStatusLoginJson(@Context UriInfo info, @PathParam ("paramJsonUser") String paramJsonUser){
+		String result = "NOK";
+		String json = info.getPathParameters().getFirst("paramJsonUser");
 		try{
 			if(json != null && json.length()>0){
 				return		UsuarioControler.validaUsuario(json);
@@ -44,4 +82,6 @@ public class UsuarioResource {
 	
 		return result;
 	}
+	
+	
 }
